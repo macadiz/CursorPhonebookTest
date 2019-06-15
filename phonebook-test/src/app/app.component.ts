fb20b92b024dfdc3c4d4967a531785e718f71d3e
Miguel Acevedo <macevedoinf@gmail.com>
2019-06-15 14:14:57 -0400

    feat: Búsqueda básica de personas ok

--- phonebook-test/src/app/app.component.ts
+++ phonebook-test/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { PersonasService } from './services/PersonasService';
+import { RegionesService } from './services/RegionesService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PersonasService, RegionesService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'phonebook-test';
+  personas: any[] = [];
+  personasDisplay = [];
+  sinResultados = false;
+
+  constructor(
+    protected personasService: PersonasService,
+    protected regionesService: RegionesService
+  ) {
+  }
+
+  ngOnInit() {
+    this.personasService.getPersonas().subscribe(
+      (data) => {
+        this.personas = data as any[];
+        console.log(this.personas);
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
+
+  buscar(form) {
+    let busquedaPersonas = this.personas.filter((p) => {
+      var nombreCompleto = p.nombre ? p.nombre + (p.apellido ? " " + p.apellido : "") : p.apellido ? p.apellido : "";
+      return nombreCompleto.toLowerCase().indexOf(form.nombre.toLowerCase()) != -1 && p.activo == 1;
+    });
+    this.sinResultados = busquedaPersonas.length == 0;
+    this.personasDisplay = busquedaPersonas != null ? busquedaPersonas : [];
+  }
+
 }

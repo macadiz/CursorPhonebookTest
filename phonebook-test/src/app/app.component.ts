63801b34fb6151e5bbbae0c63b3f80d1f43b15d0
Miguel Acevedo <macevedoinf@gmail.com>
2019-06-15 15:29:29 -0400

    fix: Transformación de codificiación de caracteres

--- phonebook-test/src/app/app.component.ts
+++ phonebook-test/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { PersonasService } from './services/PersonasService';
 import { RegionesService } from './services/RegionesService';
+import UTF8 from './utils/UTF8';
 
 @Component({
   selector: 'app-root',
@@ -24,7 +25,17 @@ export class AppComponent {
     this.personasService.getPersonas().subscribe(
       (data) => {
         this.personas = data as any[];
-        console.log(this.personas);
+        this.personas = this.personas.map((p) => {
+          p.nombre = UTF8.decode(p.nombre);
+          p.apellido = UTF8.decode(p.apellido);
+          if (p.direccion) {
+            p.direccion.calle = UTF8.decode(p.direccion.calle);
+            if (p.direccion.comuna) {
+              p.direccion.comuna.nombre = p.direccion.comuna.nombre ? UTF8.decode(p.direccion.comuna.nombre) : null;
+            }
+          }
+          return p;
+        });
       },
       (error) => {
         console.error(error);
